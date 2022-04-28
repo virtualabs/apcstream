6e0cde14dfd2ac8c57b0ebead63c4dce49c326f6
virtualabs <virtualabs@gmail.com>
2022-04-28 23:52:41 +0200

    Added auto-reconnect feature and refactored code.
    - Added a global loop in main to handle deconnection errors
    - Moved control code into control.rs module

---
+++ src/control.rs
@@ -0,0 +1,227 @@
+//! Streamdeck config management
+
+use obws::{Client,requests::{Volume, SceneItemRender}};
+use std::collections::HashMap;
+use crate::apcmini::{APCMini, LedState};
+use crate::config::DeckConfig;
+
+
+enum AudioSourceState {
+  Muted,
+  Active
+}
+
+#[derive(PartialEq)]
+enum VideoSourceState {
+  Visible,
+  Hidden
+}
+
+
+pub struct Controller {
+  apcmini: APCMini,
+  config: DeckConfig,
+  client: Client,
+  audio_sources: Vec<String>,
+  audio_states: HashMap<u8, AudioSourceState>,
+  video_sources: Vec<String>,
+  video_states: HashMap<u8, VideoSourceState>,
+  current_scene: Option<u8>
+}
+
+impl Controller {
+
+  /* "Constructor" */
+  pub async fn new(mut apcmini:APCMini, config: DeckConfig, client: Client) -> anyhow::Result<Controller> {
+    
+    /* Retrieve audio  sources from OBS websocket client. */
+    let mut audio_sources = Vec::new();
+    let mut video_sources = Vec::new();
+    let sources = client.sources().get_sources_list().await?;
+
+    /* Retrieve audio sources from OBS websocket client. */
+    for source in sources.iter().filter(|item| {(item.type_id == "pulse_input_capture") || (item.type_id == "pulse_output_capture")}) {
+      println!("Source: {:?}", source);
+      audio_sources.push(source.name.clone());
+    }
+
+    /* Retrieve video sources from OBS websocket client. */
+    for source in sources.iter().filter(|item| {item.type_id == "v4l2_input"}) {
+      println!("Source: {:?}", source);
+      video_sources.push(source.name.clone());
+    }
+
+    /* Initialize keyboard. */
+    for led in 0..64 {
+      apcmini.led_off(led);
+    }
+
+    for binding in config.bindings.scenes.iter() {
+      apcmini.set_led( binding.button, LedState::Red);
+    }
+
+    // Set audio UI and state
+    let mut audio_states = HashMap::new();
+    for binding in config.bindings.audio.iter() {
+        apcmini.set_led( binding.slider+64, LedState::Off);
+        audio_states.insert(binding.slider, AudioSourceState::Active);
+    }
+
+    let mut video_states= HashMap::new();
+    for binding in config.bindings.sources.iter() {
+      apcmini.set_led(binding.button, LedState::Yellow);
+      video_states.insert(binding.button, VideoSourceState::Visible);
+    }
+
+    /* Populate our structure. */
+    let controller = Controller {
+      apcmini: apcmini,
+      config: config,
+      client: client,
+      audio_sources: audio_sources,
+      audio_states: audio_states,
+      video_sources: video_sources,
+      video_states: video_states,
+      current_scene: None
+    };
+
+
+    /* Success. */
+    Ok(controller)
+  }
+
+  /**
+   * Dispatch button press event.
+   **/
+
+  pub async fn on_button_press(&mut self, btn_id: u8) -> anyhow::Result<()> {
+    let scene = self.config.get_scene_by_button(btn_id);
+    if let Some(scene) = scene {
+      self.switch_to_scene(scene, btn_id).await?;
+    } else {
+      if let Some(source) = self.config.get_source_by_button(btn_id) {
+        self.toggle_video_source(source, btn_id).await?;
+      }
+    }
+
+    Ok(())
+  }
+
+  pub async fn on_slider_btn_press(&mut self, btn_id: u8) -> anyhow::Result<()> {
+
+    let audio_index = btn_id - 64;
+    let audio_source = self.config.get_audiosource_by_slider(audio_index);
+    if let Some(audio_source) = audio_source {
+      self.toggle_audio_source(audio_source, btn_id).await?;
+    }
+
+    Ok(())
+  }
+
+  pub async fn on_slider_change(&mut self, slider_id: u8, slider_value: u8) -> anyhow::Result<()> {
+    let audio_source = self.config.get_audiosource_by_slider(slider_id-48);
+    if let Some(name) = audio_source {
+      self.set_volume(name, slider_value).await?;
+    }
+
+    Ok(())
+  }
+
+  pub async fn switch_to_scene(&mut self, scene: String, btn_id: u8) -> anyhow::Result<()> {
+    if self.current_scene.is_none() {
+      self.current_scene = Some(btn_id);
+    } else {
+        self.apcmini.set_led(self.current_scene.unwrap(), LedState::Red);
+        self.current_scene = Some(btn_id);
+    }
+
+    /* Exit while loop on error (OBS websocket disconnected) */
+    self.client.scenes().set_current_scene(&scene).await?;
+
+    /* Update LED state. */
+    self.apcmini.set_led( btn_id, LedState::Green);
+
+    Ok(())
+  }
+
+  pub async fn toggle_video_source(&mut self, source: String, btn_id: u8) -> anyhow::Result<()> {
+    /* If current scene is set and video_source exists. */
+    if self.video_sources.contains(&&source) && self.current_scene.is_some() {
+
+      /* Toggle video source visibility. */
+      if let Some(scene_name) =  self.config.get_scene_by_button(self.current_scene.unwrap()) {
+        if let Some(video_state) = self.video_states.get(&btn_id) {
+          let is_visible = *video_state == VideoSourceState::Hidden;
+          let scene_item = SceneItemRender {
+            scene_name: Some(&scene_name),
+            source: &source,
+            item: None,
+            render: is_visible
+          };
+
+          /* Set scene item render on OBS. */
+          self.client.scene_items().set_scene_item_render(scene_item).await?;
+          
+          match self.video_states.get(&btn_id) {
+            Some(VideoSourceState::Visible) => {
+              self.video_states.insert(btn_id, VideoSourceState::Hidden);
+              self.apcmini.set_led(btn_id, LedState::BlinkYellow);    
+            },
+            Some(&VideoSourceState::Hidden) => {
+              self.video_states.insert(btn_id, VideoSourceState::Visible);    
+              self.apcmini.set_led(btn_id, LedState::Yellow);
+            },
+            None => {
+            }
+          }
+        }
+      }
+    }
+
+    /* Success. */
+    Ok(())
+  }
+
+  pub async fn toggle_audio_source(&mut self, audio_source: String, btn_id: u8) -> anyhow::Result<()> {
+    let audio_index = btn_id - 64;
+
+    /* Check if source is muted */
+    if let Some(state) = self.audio_states.get(&(btn_id - 64)) {
+
+      /* Tell OBS source is muted, exit loop on error. */
+      self.client.sources().toggle_mute(&audio_source).await?;
+
+      match state {
+        AudioSourceState::Active => {
+            /* Mark as muted. */
+            self.audio_states.insert(audio_index, AudioSourceState::Muted);
+            self.apcmini.set_led(audio_index + 64, LedState::Red);
+        },
+        AudioSourceState::Muted => {
+            /* Mark as active. */
+            self.audio_states.insert(audio_index, AudioSourceState::Active);
+            self.apcmini.set_led(audio_index + 64, LedState::Off);
+        }
+      }
+    }
+
+    /* Success. */
+    Ok(())
+  }
+
+
+  pub async fn set_volume(&mut self, audio_source: String, volume: u8) -> anyhow::Result<()> {
+    let volume = Volume {
+        source : &audio_source,
+        volume : -(1.0 - f64::from(volume)/127.0)*100.0f64,
+        use_decibel: Some(true)
+    };
+
+    /* Exit while loop on error. */
+    self.client.sources().set_volume(volume).await?;
+    
+    /* Success. */
+    Ok(())
+  }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,185 +1,69 @@
 use anyhow::Result;
-use obws::{Client, requests::{Volume, SceneItemRender},};
+use obws::{Client};
 use tokio::sync::mpsc;
-use std::collections::HashMap;
+use std::{thread, time};
 
 // Load APC Mini controller.
 mod apcmini;
-use apcmini::{APCMini, LedState};
+use apcmini::{APCMini};
 
 // Load configuration helpers.
 mod config;
-use config::{DeckConfig, load_deck_config};
+use config::{load_deck_config};
 
-enum AudioSourceState {
-  Muted,
-  Active
-}
+mod control;
+use control::Controller;
 
-#[derive(PartialEq)]
-enum VideoSourceState {
-  Visible,
-  Hidden
-}
 
-fn apcmini_init(apcmini: &mut APCMini, config: &DeckConfig, audio: &mut HashMap<u8,AudioSourceState>, source: &mut HashMap<u8, VideoSourceState>) -> Result<()> {
-    // Set scenes buttons
-    for binding in config.bindings.scenes.iter() {
-        apcmini.set_led( binding.button, LedState::Red);
-    }
-
-    // Set audio UI and state
-    for binding in config.bindings.audio.iter() {
-        apcmini.set_led( binding.slider+64, LedState::Off);
-        audio.insert(binding.slider, AudioSourceState::Active);
-    }
-
-    for binding in config.bindings.sources.iter() {
-      apcmini.set_led(binding.button, LedState::Yellow);
-      source.insert(binding.button, VideoSourceState::Visible);
-    }
-
-    Ok(())
+async fn obsws_connect() -> Result<Client> {
+   // Connect to the OBS instance through obs-websocket.
+   let client = Client::connect("localhost", 4445).await?;
+   Ok(client)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut current_scene: Option<u8> = None;
-    let mut apc_audio_states = HashMap::new();
-    let mut apc_source_states = HashMap::new();
+  
+
+  /* Daemon keeps running and try to connect to remote service. */
+  loop {
 
     let (tx, mut rx) = mpsc::channel(32);
-
     let config = load_deck_config()?;
+    let apcmini = APCMini::new(tx)?; 
 
-    // Connect to our APC Mini.
-    let mut apcmini = APCMini::new(tx)?;
+    /* Connect to the OBS instance through obs-websocket. */
+    if let Ok(client) = obsws_connect().await {
+      let mut controller = Controller::new(apcmini, config, client).await?;
+      
+      /* Main loop, process messages. */
+      while let Some(message) = rx.recv().await {
+          match (message[0], message[1]) {
+              (0x90, 0...63) => {
+                if controller.on_button_press(message[1]).await.is_err() {
+                  break;
+                }
+              },
 
-    // Reset all leds
-    for led in 0..64 {
-        apcmini.led_off(led);
+              (0x90, 64...71) => {
+                if controller.on_slider_btn_press(message[1]).await.is_err() {
+                  break;
+                }
+              },
+
+              (0xB0, 48...57) => {
+                if controller.on_slider_change(message[1]-48, message[2]).await.is_err() {
+                  break;
+                }
+              },
+              _ => {}
+          }
+      }
+
+      println!("[!] Exited main loop (server dropped connection)");
+    } else {
+      println!("[!] Cannot connect to OBS Websocket");
+      thread::sleep(time::Duration::from_secs(1));
     }
-
-    // Configure leds based on config
-    let _ = apcmini_init(&mut apcmini, &config, &mut apc_audio_states, &mut apc_source_states);
-
-    // Connect to the OBS instance through obs-websocket.
-    let client = Client::connect("localhost", 4445).await?;
-
-    // List sources
-    let sources = client.sources().get_sources_list().await?;
-    let mut audio_sources = Vec::new();
-    for source in sources.iter().filter(|item| {(item.type_id == "pulse_input_capture") || (item.type_id == "pulse_output_capture")}) {
-        println!("Source: {:?}", source);
-        audio_sources.push(&source.name);
-    }
-
-    let mut video_sources = Vec::new();
-    for source in sources.iter().filter(|item| {item.type_id == "v4l2_input"}) {
-      println!("Source: {:?}", source);
-      video_sources.push(&source.name);
   }
-  
-    // Retrieve current scene
-    match client.scenes().get_current_scene().await {
-        Ok(scene) => {
-            let button_id = config.get_button_by_scene(scene.name);
-            if let Some(id) = button_id {
-                current_scene = Some(id);
-                apcmini.set_led(id, LedState::Green);
-            }
-        },
-        Err(_) => {
-            current_scene = None;
-        }
-    }
-
-    /* Main loop, process messages. */
-    while let Some(message) = rx.recv().await {
-        match (message[0], message[1]) {
-            (0x90, 0...63) => {
-                let scene = config.get_scene_by_button(message[1]);
-                if let Some(scene) = scene {
-                    if current_scene.is_none() {
-                        current_scene = Some(message[1]);
-                    } else {
-                        apcmini.set_led(current_scene.unwrap(), LedState::Red);
-                        current_scene = Some(message[1]);
-                    }
-
-                    client.scenes().set_current_scene(&scene).await?;
-                    apcmini.set_led( message[1], LedState::Green);
-                } else {
-                  /* TODO: refactoring ! */
-                  if let Some(source) = config.get_source_by_button(message[1]) {
-                    if video_sources.contains(&&source) && current_scene.is_some() {
-                      if let Some(scene_name) =  config.get_scene_by_button(current_scene.unwrap()) {
-                        if let Some(video_state) = apc_source_states.get(&message[1]) {
-                          let is_visible = *video_state == VideoSourceState::Hidden;
-                          let scene_item = SceneItemRender {
-                            scene_name: Some(&scene_name),
-                            source: &source,
-                            item: None,
-                            render: is_visible
-                          };
-                          client.scene_items().set_scene_item_render(scene_item).await?; 
-                          match apc_source_states.get(&message[1]) {
-                            Some(VideoSourceState::Visible) => {
-                              apc_source_states.insert(message[1], VideoSourceState::Hidden);
-                              apcmini.set_led(message[1], LedState::BlinkYellow);    
-                            },
-                            Some(&VideoSourceState::Hidden) => {
-                              apc_source_states.insert(message[1], VideoSourceState::Visible);    
-                              apcmini.set_led(message[1], LedState::Yellow);
-                            },
-                            None => {
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-            },
-            (0x90, 64...71) => {
-                let audio_index = message[1] - 64;
-                let audio_source = config.get_audiosource_by_slider(audio_index);
-                if let Some(audio_source) = audio_source {
-                    /* Check if source is muted */
-                    if let Some(state) = apc_audio_states.get(&(message[1] - 64)) {
-                        /* Tell OBS source is muted. */
-                        client.sources().toggle_mute(&audio_source).await?;
-
-                        match state {
-                            AudioSourceState::Active => {
-                                /* Mark as muted. */
-                                apc_audio_states.insert(audio_index, AudioSourceState::Muted);
-                                apcmini.set_led(audio_index + 64, LedState::Red);
-                            },
-                            AudioSourceState::Muted => {
-                                /* Mark as active. */
-                                apc_audio_states.insert(audio_index, AudioSourceState::Active);
-                                apcmini.set_led(audio_index + 64, LedState::Off);
-                            }
-                        }
-                    }
-                }
-            },
-            (0xB0, 48...57) => {
-                //println!("volume: {:?}", 20.0 * (f64::from(message[2])/127.));
-                let audio_source = config.get_audiosource_by_slider(message[1]-48);
-                if let Some(name) = audio_source {
-                    let volume = Volume {
-                        source : &name,
-                        volume : -(1.0 - f64::from(message[2])/127.0)*100.0f64,
-                        use_decibel: Some(true)
-                    };
-                    client.sources().set_volume(volume).await?;
-                }
-            },
-            _ => {}
-        }
-    }
-    
-    Ok(())
 }
